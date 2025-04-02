2c0fc5e5a15e135d816121fa811899155137232f
hikida <ponponda0103@gmail.com>
2025-04-02 21:16:10 +0900

    model追加

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,13 +1,19 @@
 class User{
   //プロパティ
   final String id;
-  final String progileImageUrl;
+  final String profileImageUrl;
   
   //コンストラクタ
   User({
     required this.id,
-    required this.progileImageUrl,
+    required this.profileImageUrl,
   });
-
+  // JSONからUserを生成するファクトリコンストラクタ
+  factory User.fromJson(dynamic json) {
+    return User(
+      id: json['id'] as String,
+      profileImageUrl: json['profile_image_url'] as String,
+    );
+  }
   
 }
